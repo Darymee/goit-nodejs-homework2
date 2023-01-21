1a4fc75d97c37f48ba84d59c29f6331e6348fa5f
Darymee <danieledaniele3005@gmail.com>
2023-01-21 19:20:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,18 +16,17 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use("/users", authRouter);
 
-app.use((error, req, res, next) => {
-  if (error.name === "ValidationError") {
-    return res.status(400).json({ message: error.message });
-  }
-});
-
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((error, req, res, next) => {
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ message: error.message });
+  }
+  return res
+    .status(500)
+    .json({ message: "Something went wrong, please try again later..." });
 });
 
 module.exports = app;
